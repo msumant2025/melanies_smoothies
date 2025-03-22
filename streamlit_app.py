c96a2ad68f2c5a1566ec5b6f4a05e2db382a719e
msumant2025 <msumant@yahoo.com>
2025-03-22 10:40:57 +0530

    search on added

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ st.write("The name on your somoothie will be ", name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
 
 ingredients_list = st.multiselect( 'Choose up to 5 ingredients', my_dataframe, max_selections=5)
 if ingredients_list:
