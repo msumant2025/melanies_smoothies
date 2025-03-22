57bf107a04142f0000bc267d75ed1a5408b18006
msumant2025 <msumant@yahoo.com>
2025-03-22 10:46:02 +0530

    stop added

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 import requests
+import pandas
 
 helpful_links = [
     "https://docs.streamlit.io",
@@ -22,6 +23,8 @@ st.write("The name on your somoothie will be ", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
+st.dataframe (data=my_dataframe, use_container_width=True)
+st.stop()
 
 ingredients_list = st.multiselect( 'Choose up to 5 ingredients', my_dataframe, max_selections=5)
 if ingredients_list:
