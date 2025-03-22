5d83da7d7f2b8347434845caac71ccf5d467c07a
msumant2025 <msumant@yahoo.com>
2025-03-22 11:10:27 +0530

    comment

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,8 +32,8 @@ st.dataframe(pd_df)
 
 ingredients_list = st.multiselect( 'Choose up to 5 ingredients', my_dataframe, max_selections=5)
 if ingredients_list:
-    st.write(ingredients_list)
-    st.text(ingredients_list)
+    #st.write(ingredients_list)
+    #st.text(ingredients_list)
 
     ingredients_string = ''
 
